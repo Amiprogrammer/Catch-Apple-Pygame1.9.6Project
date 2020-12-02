d1c899e0cd46169d36a16ebe9942a6040f04c4f0
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-12-02 15:28:43 +0900

    initialize with by importing pygame library and description for this program

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+author: Juliao Martins
+github account: https://www.github.com/amiprogrammer/
+
+CATCH APPLE WITH PYGAME 1.9.6
+
+NOTE:
+    i'm build this game cause, i want to improve my skill in pygame library to develop game and i want to make something new with pygame library.
+"""
+
+import pygame
+from pygame.locals import *
