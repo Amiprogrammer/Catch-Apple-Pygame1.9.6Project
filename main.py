9f7c4e7c93c0031ae5c3766f1b58558985c5d6ae
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-12-02 15:47:06 +0900

    Set a bowl and movement wit keyboard pressed

--- main.py
+++ main.py
@@ -23,6 +23,15 @@ pygame.display.set_caption("Catch Apple")
 # background color
 darkgray = pygame.Color("darkgray")
 
+# bowl here
+bowl_img = pygame.image.load("img/fruit-bowl.png")
+bowl_x = 390
+bowl_y = 570
+bowl_x_change = 0
+def bowl(x,y):
+    global screen,bowl_img
+    screen.blit(bowl_img,(x,y))
+
 running = True  
 while running:
 
@@ -31,6 +40,23 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                bowl_x_change = -1
+            elif event.key == K_RIGHT:
+                bowl_x_change = 1
+        elif event.type == KEYUP:
+            if event.key == K_LEFT or event.key == K_RIGHT:
+                bowl_x_change = 0
+
+    # boundaries of bowl
+    if bowl_x <= 10:
+        bowl_x = 10
+    elif bowl_x >= 756:
+        bowl_x = 756
+
+    bowl_x += bowl_x_change
+    bowl(bowl_x,bowl_y)
 
     pygame.display.update()
 
